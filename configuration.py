6265acde9ee572f665a3d84da2e950a0159c28b7
Kevin Gudiño <kevingtest24@live.com.mx>
2026-03-28 13:07:32 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+import services
+
+URL_SERVICE = "https://cnt-35a0e00d-1de7-4f99-abae-94e6c538a81b.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
